469ee20602428126fb75badaec7ec21d280e8ff5
FujiiNonchan <quercus@pearl.ocn.ne.jp>
2022-05-20 06:02:35 +0900

    演習の解答例

--- test_selenium.py
+++ test_selenium.py
@@ -2,15 +2,43 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import chromedriver_binary
+import time
+from selenium.webdriver.common.by import By
+from openpyxl import Workbook
+
 
 option = Options()
 option.add_argument('--headless')
 
 driver = webdriver.Chrome(options=option)
+
 driver.get('https://www.google.co.jp/')
+
+# 検索フィールドの取得
+query = driver.find_element(by=By.NAME, value='q')
+
+# 検索文字列を入力
+query.send_keys('クローリング')
+
+# 3秒待つ
+time.sleep(3)
+
+# 検索ボタンをクリック
+button = driver.find_element(by=By.NAME, value='btnK')
+button.click()
+
+# 3秒待つ
+time.sleep(3)
+
+# リンクの一覧を取得
 html = driver.page_source
 soup = BeautifulSoup(html, 'html.parser')
 
-ll = [x for x in soup.text.split(' ') if len(x) > 0]
-for elem in ll:
-    print(elem)
+# 取得したリンクの一覧をExcelに出力する
+wb = Workbook()
+ws = wb.active
+
+for index, a in enumerate(soup.find_all('a')):
+    ws.cell(row=index + 1, column=1).value = a.get('href')
+
+wb.save("answer.xlsx")
